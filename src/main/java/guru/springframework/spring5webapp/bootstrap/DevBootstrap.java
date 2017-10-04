cade2fbcc036d9cc38da32526e59b40a75939726
John Thompson <john@springframework.guru>
2017-10-04 08:35:32 -0400

    fix to Rod mapping

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -43,6 +43,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
         rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
