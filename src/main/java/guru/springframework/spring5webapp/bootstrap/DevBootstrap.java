ccb63efbe91d4c4f15165f65c4dc6db98c818cc3
springframeworkguru <john@springframework.guru>
2017-12-10 07:17:56 +0530

    Updated to 2.0.0.M7

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -35,7 +35,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         Publisher publisher = new Publisher();
         publisher.setName("foo");
-
+        publisher.setAddress("12th Street, LA");
         publisherRepository.save(publisher);
 
         //Eric
