7cefcc0075d1436eabf0ae2769ef297464eb1096
Jeff Thomas <jeff.w.thomas@outlook.com>
2018-06-25 00:52:40 +0200

    Added @Repository annotation to Repository Beans to avoid Autowire warning in Controllers

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -2,9 +2,11 @@ package guru.springframework.spring5webapp.repositories;
 
 import guru.springframework.spring5webapp.model.Author;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by jt on 5/16/17.
  */
+@Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
