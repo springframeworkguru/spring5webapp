42b53f5ff670c215c681cde966188087f5ab250c
mtkk <mm@gmail.com>
2018-10-12 23:09:42 +0200

    added test 2ss

---
+++ src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests2.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring5webapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5webappApplicationTests2 {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void contextLoads2() {
+	}
+}
