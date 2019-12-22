1dec5f2c343a047cbfdfe2e7c5677e0e46b16f93
John Thompson <john@springframework.guru>
2019-12-22 14:17:57 -0500

    adding new Spring Boot 2.2 base project

---
+++ src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
