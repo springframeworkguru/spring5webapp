f407e7b8aeebeb621d33bfe3a1329f5a8738a310
John Thompson <john@springframework.guru>
2019-12-24 12:25:18 -0500

    adding books view and thymeleaf

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -22,6 +22,6 @@ public class BookController {
 
         model.addAttribute("books", bookRepository.findAll());
 
-        return "books";
+        return "books/list";
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -79,7 +79,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
