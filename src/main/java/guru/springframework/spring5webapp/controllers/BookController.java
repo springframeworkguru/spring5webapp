33ea8f2369a12d317a541eb2850ca388d8e09154
Subham Tripathi <tripathi01subham@gmail.com>
2020-04-02 00:39:44 +0530

    refactoring : thymeleaf intro

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -15,7 +15,7 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @RequestMapping("/books")
+    @RequestMapping("templates/books")
     public String getBooks(Model model){        //model will return the result to the View and in view we will decide how to return the data
     model.addAttribute("books",bookRepository.findAll());   //model will have an attribute books and it will contain the list of books
     return "books/list";
