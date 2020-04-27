cdec7d6ee756ded0753faa174879a6bfad76d01b
vikassurana11 <49195728+vikassurana11@users.noreply.github.com>
2020-04-27 13:22:30 +0530

    Spring first app
    Spring first app (complete)

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,47 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+	
+	private final AuthorRepository authorRepository;
+	private final BookRepository bookRepository;
+	private final PublisherRepository publisherRepository;
+	
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Author eric = new Author("Eric", "Evans");
+		Book ddd = new Book("Domain Driven Design", "123123");
+		Publisher publisher = new Publisher("Shweta", "CTC", "Secunderabad", "Telangana", "500015");
+		eric.getBooks().add(ddd);
+		ddd.getAuthors().add(eric);
+		ddd.setPublisher(publisher);
+		publisher.getBooks().add(ddd);
+		
+		authorRepository.save(eric);
+		bookRepository.save(ddd);
+		publisherRepository.save(publisher);
+		
+		System.out.println("Started bootstrap");
+		System.out.println("Number of books: "+bookRepository.count());
+		System.out.println("Publisher count: "+publisherRepository.count());
+		System.out.println("Publisher Number of books: "+publisher.getBooks().size());
+	}
+
+}
