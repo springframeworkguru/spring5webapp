1cf4fe27bce90c5b178ddbdfd0edae271b46bf5c
vvoitsikhovskyi <vvoitsikhovskyi@corevalue.net>
2020-06-05 17:27:12 +0300

    17. Initializing Data with Spring

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -13,12 +14,12 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
-    public Author(String firstName, String lastName, Set<Book> books) {
+    public Author(String firstName, String lastName/*, Set<Book> books*/) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.books = books;
+        //this.books = books;
     }
 
     public Author() {
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -13,13 +14,14 @@ public class Book {
     private String isbn;
 
     @ManyToMany
-    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name="book_id"),
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn/*, Set<Author> authors*/) {
         this.title = title;
         this.isbn = isbn;
+//        this.authors = authors;
     }
 
     public Book() {
