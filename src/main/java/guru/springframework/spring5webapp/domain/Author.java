d209a16f4c2a0819f1fc838b952b6dee88510035
Laure-di <masson.laure.l@gmail.com>
2020-07-21 21:14:12 +0200

    class Author and Book Creation

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,5 @@
+package guru.springframework.spring5webapp.domain;
+
+public class Author {
+
+}
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5webapp.domain;
+
+@Entity
+
+public class Book {
+	
+}
