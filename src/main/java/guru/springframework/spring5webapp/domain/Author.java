8e690ed3f9976bb0c5b78c458251eaefe860cfe6
Laure-di <masson.laure.l@gmail.com>
2020-07-22 10:29:16 +0200

    implementation of Author and Book Repositories

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,5 +1,68 @@
 package guru.springframework.spring5webapp.domain;
 
-public class Author {
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String lastName;
+	private String firstName;
+	private Set<Book> books;
+	
+	
+	public Author(String lastName, String firstName, Set<Book> books) {
+		super();
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.books = books;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+	
 }
