1ac4f624e5fd1fe2119d7a9f0220292805ab9f37
silviu.popescu <constantinsilviupopescu@gmail.com>
2020-11-06 15:09:16 +0200

    commit Publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -5,41 +5,74 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+
+/**
+ * 
+ * @author silviu.popescu
+ * @category Initializing Data with Spring (lesson 17)
+ *
+ */
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 	
 	private final AuthorRepository authorRepository;
 	private final BookRepository bookRepository;
+	private final PublisherRepository publisherRepository;
 	
-	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
 		
+		System.out.println("Started in Bootstrap");
+
+		Publisher publisher = new Publisher();
+		publisher.setName("SFG Publishing");
+		publisher.setCity("St. Petersburg");
+		publisher.setState("FL");
+		publisher.setAddressLine1("str. no");
+		publisher.setZip("1234");
+		
+		publisherRepository.save(publisher);
+		System.out.println("Publisher Count: " + publisherRepository.count());
+				
 		Author eric = new Author("Eric", "Evans");
 		Book ddd = new Book("Domain Driven Design","123123");
 		eric.getBooks().add(ddd);
 		ddd.getAuthors().add(eric);
 		
+		ddd.setPublisher(publisher);
+		publisher.getBooks().add(ddd);
+		
 		authorRepository.save(eric);
 		bookRepository.save(ddd);
+		publisherRepository.save(publisher);
 		
 		Author rod = new Author("Rod", "Johnson");
 		Book noEJB = new Book("J2EE Development without EJB", "3939459459");
 		rod.getBooks().add(noEJB);
 		noEJB.getAuthors().add(rod);
 		
-		System.out.println("Started in Bootstrap");
+		noEJB.setPublisher(publisher);
+		publisher.getBooks().add(noEJB);
+		
+		authorRepository.save(rod);
+		bookRepository.save(noEJB);
+		publisherRepository.save(publisher);
+		
 		System.out.println("Number of books:" + bookRepository.count());
+		System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
 	}
 
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -13,6 +13,9 @@ public class Book {
 
     private String title;
     private String isbn;
+    
+    @ManyToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -26,8 +29,18 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
     }
+    
+    
 
-    public String getTitle() {
+    public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getTitle() {
         return title;
     }
 
