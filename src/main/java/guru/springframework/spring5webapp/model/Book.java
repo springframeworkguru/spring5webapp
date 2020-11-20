3f9e9cf289b4bb9bfd2db81b48dd636103e5ed16
Christopher DiStasio <cadistasio@gmail.com>
2020-11-19 18:48:36 -0600

    Completes book pojo

---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,54 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+
+    )
+    private Set<Author> authors;
+
+    public Book() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Book(String title, String isbn, Set<Author> author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+}
