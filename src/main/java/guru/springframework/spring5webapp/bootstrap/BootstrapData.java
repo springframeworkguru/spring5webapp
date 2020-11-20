8370b36427c288e8d5c12159d5e28725b210d37b
Christopher DiStasio <cadistasio@gmail.com>
2020-11-20 11:46:50 -0600

    Tests running author, book pojos through their repositories

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -1,5 +1,7 @@
 package guru.springframework.spring5webapp.bootstrap;
 
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -19,5 +21,23 @@ public class BootstrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Author john = new Author("john", "steinbeck");
+        Book tgw = new Book("The Grapes of Wrath", "1111111111111");
+        john.getBooks().add(tgw);
+        tgw.getAuthors().add(john);
+
+        authorRepository.save(john);
+        bookRepository.save(tgw);
+
+        Author frank = new Author("frank", "herbert");
+        Book dune = new Book("Dune", "2222222222222");
+        frank.getBooks().add(dune);
+        dune.getAuthors().add(frank);
+
+        authorRepository.save(frank);
+        bookRepository.save(dune);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of books: " + bookRepository.count());
     }
 }
