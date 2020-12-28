b2abf2f9170e1fb6e54cdb0f8c1441e8e97f5b88
DAVID ALONSO TORREJON <DAVID.ALONSO.TORREJON@ing.com>
2020-12-28 20:19:03 +0100

    adding thymeleaf template & author controller

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorControllers.java
@@ -0,0 +1,2 @@
+package guru.springframework.spring5webapp.controllers;public class AuthorControllers {
+}
---
+++ src/main/java/guru/springframework/spring5webapp/entities/Author.java
@@ -0,0 +1,2 @@
+package guru.springframework.spring5webapp.entities;public class Author {
+}
