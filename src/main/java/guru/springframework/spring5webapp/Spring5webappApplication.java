3765bbd14b49122ca75d93b2480f9ceb907e1164
farookwadia <farookwadia@10.0.0.4>
2020-12-27 21:29:05 -0500

    test comment

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
+
+		// a test
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
 }
