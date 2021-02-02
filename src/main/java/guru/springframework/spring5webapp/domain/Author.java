9bb6775d542c72f17ed4dc6a490a9e0e536166e3
Shawn Bland <malibu368@gmail.com>
2021-02-01 18:24:59 -0600

    Assignment 2 with Thymeleaf complete

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -119,7 +119,6 @@ public class Author {
 				"id=" + id + 
 				", firstName'" + firstName + '\'' + 
 				", lastName'" + lastName + '\'' + 
-				", books='" + books +
 				'}';
 	}
 	
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -104,7 +104,6 @@ public class Book {
 				"id=" + id + 
 				", title'" + title + '\'' + 
 				", isbn'" + isbn + '\'' + 
-				", authors='" + authors +
 				'}';
 	}
 
