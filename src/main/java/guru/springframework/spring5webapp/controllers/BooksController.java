eba3fff20b4d76e095da4bd0e3f2592e3359d615
Elad Alfa <eladalfa@gmail.com>
2021-02-07 20:49:16 +0200

    Add books controller

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BooksController.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+
+@Controller
+public class BooksController {
+
+	private BookRepository repo;
+	
+	
+	
+	public BooksController(BookRepository repo) {
+		this.repo = repo;
+	}
+
+
+	@GetMapping("/books")
+	public String getBooks(Model model) 
+	{
+		model.addAttribute("books", repo.findAll());
+		
+		return "books";
+	}
+	
+	
+}
