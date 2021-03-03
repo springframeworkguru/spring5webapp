0eefa919d8285002da1b3da149623e8a9d726200
victorandruscabit <victor.v.andrusca@gmail.com>
2021-03-03 10:56:52 +0200

    added repositories iterface, and initilezed via bootstrap process

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -14,7 +15,7 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book>books;
+    private Set<Book>books = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -61,10 +62,9 @@ public class Author {
                 '}';
     }
 
-    public Author(String firstName, String lastName, Set<Book> books) {
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.books = books;
     }
 
     @Override
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -8,19 +9,17 @@ public class Book {
     @Id
     @GeneratedValue( strategy = GenerationType.AUTO)
     private  Long id;
-
     private String title;
     private String isbn;
 
     @ManyToMany
     @JoinTable( name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id") )
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn){
         this.title = title;
         this.isbn = isbn;
-        this.authors = authors;
     }
 
     public Book() {
