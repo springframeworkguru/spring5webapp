31f9f6bdd0d058519f9dc083a38fe4fcb5947bb0
mohdsalman <mohd.salman@cimpress.com>
2021-04-02 01:06:40 +0530

    FEATURE: Adding thymeleaf template engine

---
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5webapp.repositories;
+
+import guru.springframework.spring5webapp.domain.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
