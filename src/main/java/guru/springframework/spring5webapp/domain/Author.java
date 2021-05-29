d1d86005004fe3dc34dd03195e60d3281da66cb7
CFerraris <catherine_ferraris42@hotmail.com>
2021-05-28 21:22:51 -0400

    Add many-to-many between author and book

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Author {
@@ -16,6 +17,8 @@ public class Author {
 
 	private String firstName;
 	private String lastName;
+
+	@ManyToMany(mappedBy = "authors")
 	private Set<Book> Books;
 
 	public Author() {
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Book {
@@ -16,6 +19,9 @@ public class Book {
 
 	private String title;
 	private String isbn;
+
+	@ManyToMany
+	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
 	private Set<Author> authors;
 
 	public Book() {
