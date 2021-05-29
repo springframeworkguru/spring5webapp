0b8e9e7d3562787f758472ff694967cca7cb9bae
CFerraris <45601211+CFerraris@users.noreply.github.com>
2021-05-28 21:38:46 -0400

    14 jpa entities (#1)
    * Add author and book class
    
    * Add many-to-many between author and book

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,57 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String firstName;
+	private String lastName;
+
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> Books;
+
+	public Author() {
+	}
+
+	public Author(String firstName, String lastName, Set<Book> books) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		Books = books;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Set<Book> getBooks() {
+		return Books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		Books = books;
+	}
+
+}
