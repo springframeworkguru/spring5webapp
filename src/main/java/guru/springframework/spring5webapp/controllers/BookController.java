23ca38cc082916d18285267eecdc998233a974e1
mchojniak <mchojniak@gabos.pl>
2021-06-01 12:14:04 +0200

    @Controller annotation fix

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,10 +1,11 @@
 package guru.springframework.spring5webapp.controllers;
 
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 public class BookController {
 
     private final BookRepository bookRepository;
