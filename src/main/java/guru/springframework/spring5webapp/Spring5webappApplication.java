7553ad973af03cf3e5532d0ddda07a78ce8cf81f
Grigor Grigoryan <grigor.grigoryan@webbfontaine.com>
2021-06-24 11:44:14 +0400

    Test

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5webappApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
