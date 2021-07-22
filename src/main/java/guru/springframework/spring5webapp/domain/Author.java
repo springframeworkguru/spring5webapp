8b3dd4301ad9102d85c97f551632f6bb4c7f5bef
Jim Strawn <jdstrawn@gmail.com>
2021-07-21 21:27:49 -0500

    Add model pojos

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,43 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+
+public class Author {
+
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
