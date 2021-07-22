88b1e1f067514e1a2bbbb6ec85c44cddbf6f708e
Jim Strawn <jdstrawn@gmail.com>
2021-07-21 21:34:33 -0500

    Make pojos JPA Entities

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,8 +1,17 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String title;
     private String isbn;
@@ -15,6 +24,14 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
