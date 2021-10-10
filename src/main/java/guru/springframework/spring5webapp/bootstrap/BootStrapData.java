3c04143f6a33da51717bb863025098345389d996
Lee, Kyungsoo | Will <kyungsoo.lee@rakuten.com>
2021-10-10 23:04:26 +0900

    initializing-data-with-spring

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,44 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evans");
+        Book bbb = new Book("Domain Driven Design", "123123");
+
+        eric.getBooks().add(bbb);
+        bbb.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(bbb);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "123456789");
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+    }
+}
