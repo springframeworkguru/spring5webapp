81029dcbf4a38e7d99cb7e14e7045ae0ade3acc8
Lee, Kyungsoo | Will <kyungsoo.lee@rakuten.com>
2021-10-25 21:58:58 +0900

    1-22-Thymeleaf-Templates

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -18,6 +18,6 @@ public class BookController {
   public String getBooks(Model model) {
 
     model.addAttribute("books", bookRepository.findAll());
-    return "books";
+    return "books/list";
   }
 }
