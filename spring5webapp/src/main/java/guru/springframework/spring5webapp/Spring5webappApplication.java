618de9e3148f7b3d141c7989544bb7cf08c8625f
Asit Kumar Sahoo <33388707+AsitCruz@users.noreply.github.com>
2021-12-18 19:59:26 +0530

    Changed in md file

---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+}
