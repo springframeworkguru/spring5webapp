7b33a0f6c9b09759a498cf1edf55ece758040702
Sam Rossilli <Sarossilli@gmail.com>
2021-12-21 15:59:50 -0700

    Initialize Objects

---
+++ src/main/java/guru/springframework/spring5webapp/Book.java
@@ -0,0 +1,42 @@
+package guru.springframework.spring5webapp;
+
+import java.util.Set;
+
+public class Book {
+    private String title;
+    private  String isbn;
+    private Set<Author> authors;
+
+    public Book(String title, String isbn, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
