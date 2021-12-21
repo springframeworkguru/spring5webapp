6ed5f9541000f14872f4dbf4527c6c07e05d36f2
Sam Rossilli <Sarossilli@gmail.com>
2021-12-21 16:13:30 -0700

    JPA Entities set up. Override toString, HashCode, and Equality.

--- src/main/java/guru/springframework/spring5webapp/Author.java
+++ src/main/java/guru/springframework/spring5webapp/Author.java
@@ -1,11 +1,19 @@
 package guru.springframework.spring5webapp;
 
+import javax.persistence.*;
 import java.util.Set;
+
+@Entity
 public class Author {
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
@@ -14,6 +22,14 @@ public class Author {
     }
 
     public Author() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -38,5 +54,25 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString(){
+        return firstName + " " + lastName;
     }
 }
--- src/main/java/guru/springframework/spring5webapp/Book.java
+++ src/main/java/guru/springframework/spring5webapp/Book.java
@@ -1,11 +1,23 @@
 package guru.springframework.spring5webapp;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Book {
+
     private String title;
     private  String isbn;
+
+    @ManyToMany
+    @JoinTable(name= "author_book",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name="author_id"))
     private Set<Author> authors;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     public Book(String title, String isbn, Set<Author> authors) {
         this.title = title;
@@ -14,6 +26,14 @@ public class Book {
     }
 
     public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -38,5 +58,25 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return title;
     }
 }
