8de383df353905be2b65050833e14ba666c3f075
Isan Sahoo <isansahoo@isans-mbp.lan>
2021-12-25 01:15:46 -0800

    Added publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,23 +14,32 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Started bootstrap...");
+
         Author isan = new Author("Isan", "Sahoo");
         Book test = new Book("TestBook", "123abc");
         isan.getBooks().add(test);
         test.getAuthors().add(isan);
 
+        Publisher publisher = new Publisher();
+        publisher.setName("Test publisher");
+        publisher.setCity("Cupertino");
+        publisherRepository.save(publisher);
+
         authorRepository.save(isan);
         bookRepository.save(test);
 
-        System.out.println("Started bootstrap...");
         System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
     }
 }
