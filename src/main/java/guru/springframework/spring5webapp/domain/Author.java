b411edea00d597a0a28779e10a3c47cae37e6e7e
Francisco Madrigal <francisco.madrigal@vipera.com>
2022-09-02 12:48:06 +0200

    lesson 22

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -66,7 +66,6 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 
