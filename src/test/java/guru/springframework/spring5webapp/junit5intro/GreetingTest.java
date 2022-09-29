70d75216ba413024dd3334acdb503e7fd1cbb5f0
milan.szalai <milan.szalai@ivm-micrologistics.com>
2022-09-29 10:01:48 +0200

    spring5-testing fix jupiter version

---
+++ src/test/java/guru/springframework/spring5webapp/junit5intro/GreetingTest.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring5webapp.junit5intro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GreetingTest {
+
+    @Test
+    void helloWorld() {
+    }
+
+    @Test
+    void testHelloWorld() {
+    }
+}
