687c5ee6d4f6cef6ded92a047e5da10d1cc4c98b
milan.szalai <milan.szalai@ivm-micrologistics.com>
2022-09-29 10:38:16 +0200

    section4-greeting tests

--- src/test/java/guru/springframework/spring5webapp/junit5intro/GreetingTest.java
+++ src/test/java/guru/springframework/spring5webapp/junit5intro/GreetingTest.java
@@ -2,15 +2,43 @@ package guru.springframework.spring5webapp.junit5intro;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
 
-    @Test
-    void helloWorld() {
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("___before all___");
+    }
+
+    Greeting greeting;
+    @BeforeEach
+    void setUp() {
+        System.out.println("...before each...");
+        greeting = new Greeting();
     }
 
     @Test
-    void testHelloWorld() {
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void helloWorldWithName() {
+        System.out.println(greeting.helloWorld("Milan"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("...after each...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("___after all___");
     }
 }
