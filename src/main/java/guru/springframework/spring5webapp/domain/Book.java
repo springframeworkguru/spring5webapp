b69cfba85f64f74188204e17dbe3391e29291f50
Fiagu <jonatan.lisek@gmail.com>
2022-10-07 13:19:37 +0200

    Added equals and toString
    and testing branches

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp.domain;
 
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 
@@ -57,5 +58,30 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
