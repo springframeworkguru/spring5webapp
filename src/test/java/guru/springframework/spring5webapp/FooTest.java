1c1f2ce38f55e8d28f3e815abfe7d4e8477e02cf
Luis Rosales <guama1239@gmail.com>
2022-10-13 12:33:57 -0500

    test on these two files foo and FooTest

---
+++ src/test/java/guru/springframework/spring5webapp/FooTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FooTest {
+
+    @Test
+    void getBar() {
+        Foo foo = new Foo();
+
+        String result = foo.getBar();
+
+        assertEquals("FooBar", result);
+    }
+}
