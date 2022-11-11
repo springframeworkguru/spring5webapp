cd0b237479493aafce8c96d163449649fc29cb0f
shinospp <ppshinos@gmail.com>
2022-11-11 23:12:03 +0530

    Hello Controller

---
+++ src/main/java/guru/springframework/spring5webapp/controller/HelloController.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring5webapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello world!!!";
+	}
+
+}
