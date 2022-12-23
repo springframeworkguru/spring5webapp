9d0ab57da8956290030b59e23b8086af3543f0eb
Sofimedina <sofi.k.medina@gmail.com>
2022-12-22 23:11:55 -0300

    adding equality
    Signed-off-by: Sofimedina <sofi.k.medina@gmail.com>

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -68,4 +68,30 @@ public class Book {
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
     }
+
+    @Override
+    public String   toString() {
+        return "Book{" +
+                "id=" + id +
+                ", tittle='" + tittle + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id.equals(book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
