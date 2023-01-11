0b952ec0f933663ef280d7cd48094bbf67301c3a
Ahmed laazouzi <laaz.braid@hotmail.fr>
2023-01-11 09:38:25 -0500

    add author controller

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/AuthorController.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+    @RequestMapping("/authors")
+            public String getAuthors(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+        return"authors/authorsList";
+            }
+}
