05bb007d5662b2e1c59bfcfb5cee3e2abf742e2f
hasinthajanaka <hasinthajanaka@gmail.com>
2023-02-08 08:57:42 +0530

    Create test.java
    Testing

---
+++ src/test.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class Test {
+
+	public string Test() {
+		return "Hello world";
+	}
+}
