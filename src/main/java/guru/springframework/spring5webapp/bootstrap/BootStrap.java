584bc76954f4560620c14c06b75af15b96c37414
AndrewBergstrom <andrew.bergstrom88@gmaill.com>
2023-02-27 17:13:39 -0700

    Created publisher bootstrap data, publisherRepository as an interface, and a publisher class with properties of name, address, city, state, zip.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
-import guru.springframework.spring5webapp.repositories.BookRepositroy;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,23 +13,44 @@ import org.springframework.stereotype.Component;
 public class BootStrap implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
-    private final BookRepositroy bookRepositroy;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrap(AuthorRepository authorRepository, BookRepositroy bookRepositroy) {
+    // Constructor that tells Spring Framework we want instances injected into class using dependency injection
+    public BootStrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
-        this.bookRepositroy = bookRepositroy;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Desing", "123123");
 
+        System.out.println("Started in Bootstrap");
+
+        // Creating NEW publisher object
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+        // Creating new Author object
+        Author eric = new Author("Eric", "Evans");
+
+        // Creating new Book object
+        Book ddd = new Book("Domain Driven Design", "123123");
+
+        //adding book to Set in Author Class
         eric.getBooks().add(ddd);
+        //adding author to Set in Book Class
         ddd.getAuthors().add(eric);
 
         authorRepository.save(eric);
-        bookRepositroy.save(ddd);
+        bookRepository.save(ddd);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJ8", "39394546");
@@ -35,10 +58,9 @@ public class BootStrap implements CommandLineRunner {
         noEJB.getAuthors().add(rod);
 
         authorRepository.save(rod);
-        bookRepositroy.save(noEJB);
+        bookRepository.save(noEJB);
 
-        System.out.println("Started in Bootstrap");
-        System.out.println("Number of Books: " + bookRepositroy.count());
+        System.out.println("Number of Books: " + bookRepository.count());
 
     }
 }
--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -3,5 +3,5 @@ package guru.springframework.spring5webapp.repositories;
 import guru.springframework.spring5webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepositroy extends CrudRepository<Book, Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 }
