37c3fd5e6d5382daf3efc6ac634d689b07c291b3
Lavrov Alex <larri250@gmail.com>
2023-03-08 19:46:35 +0200

    add views book, author

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -77,7 +77,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
