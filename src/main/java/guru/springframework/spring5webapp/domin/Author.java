8fc878b55d762dc6e2937c563aeab1104df2c214
walieldin.hassan <creaspo6@gmail.com>
2023-07-07 12:22:51 +0200

    added hash code and equals methods

--- src/main/java/guru/springframework/spring5webapp/domin/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domin/Author.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domin;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 @Entity
 public class Author {
@@ -54,5 +55,18 @@ public class Author {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domin/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domin/Book.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domin;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 @Entity
 public class Book {
@@ -54,5 +55,18 @@ public  Book()
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
