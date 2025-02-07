6d4da18dbfb7f31c223ae5cf1252cffcf4679341
bfenn <bfenn@beyond.com>
2025-02-07 13:14:24 -0700

    jpa

---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,56 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.Set;
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany("book")
+    private Set<Book> books;
+
+
+    public Author(){}
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
